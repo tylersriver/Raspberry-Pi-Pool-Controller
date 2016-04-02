8c6b9c97c1c570fd8c39563274038b9a4d32a389
Preston Kemp <preston.kemp@eagles.oc.edu>
2016-04-02 17:06:28 -0500

    Reorganization for pumpUpdate and heaterUpdate

--- scheduler/index.php
+++ scheduler/index.php
@@ -83,26 +83,62 @@ Last Revision: 8 March 2016
             </td>
         </table>
 
-        <body ng-controller="Ctrl as ctrl">
+        <body>
 
-        <div class="doublewidthTile">
-            <ng-timepicker ng-model="ctrl.time"></ng-timepicker>
-            {{ ctrl.time }}
+        <script>
+            $(function() {
+                $( "#accordion" ).accordion();
+            });
+        </script>
 
-            <ng-timepicker ng-model="ctrl.time2" theme="red" show-meridian="true"></ng-timepicker>
-            {{ ctrl.time2 }}
-
-            <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.min.js"></script>
-            <script type="text/javascript" src="/ngTimepicker-master/src/js/ngTimepicker.min.js"></script>
-
-            <script type="text/javascript">
-                var app = angular.module('app', ['jkuri.timepicker']);
-                app.controller('Ctrl', [function() {
-
-                }]);
-            </script>
-
+        <div id="accordion">
+            <h3>Section 1</h3>
+            <div>
+                <p>
+                    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+                    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+                    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+                    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+                </p>
             </div>
+            <h3>Section 2</h3>
+            <div>
+                <p>
+                    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+                    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+                    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+                    suscipit faucibus urna.
+                </p>
+            </div>
+            <h3>Section 3</h3>
+            <div>
+                <p>
+                    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+                    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+                    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+                    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+                </p>
+                <ul>
+                    <li>List item one</li>
+                    <li>List item two</li>
+                    <li>List item three</li>
+                </ul>
+            </div>
+            <h3>Section 4</h3>
+            <div>
+                <p>
+                    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+                    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+                    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+                    mauris vel est.
+                </p>
+                <p>
+                    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos.
+                </p>
+            </div>
+        </div>
 
     </div>
 
