8fc0c43c1c51f349ba59cb21099934556f4d579e
tyler.w.sriver <tyler.w.sriver@eagles.oc.edu>
2016-04-04 14:48:28 -0500

    Reorganization for pumpUpdate and heaterUpdate

--- settings/index.php
+++ settings/index.php
@@ -243,7 +243,6 @@ Last Revision: 8 March 2016
             Copyright 2016 Google Inc. All Rights Reserved.
             Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
             -->
-
     </div>
 </div>
 
